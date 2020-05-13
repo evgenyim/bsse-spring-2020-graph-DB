3f9f98603c56a09ac131f0906f56c8a75fc77bca
Evgeny Im <evgenyim6456@gmail.com>
2020-05-13 18:48:00 +0300

    refactor test_script_handler

--- test/test_script_handler.py
+++ test/test_script_handler.py
@@ -4,35 +4,31 @@ from src.script_handler import *
 from src.cyk import cyk
 
 
-def test_list_all_graphs(capsys):
-    path = os.path.dirname(__file__) + '/resources'
+def run_script(s):
     temp = tempfile.NamedTemporaryFile()
     f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'list all graphs;'.format(path + '/test_dir'))
+    f.write(s)
     f.close()
-    handle_script_from_file(temp.name)
+    return handle_script_from_file(temp.name)
+
+
+def test_list_all_graphs(capsys):
+    path = os.path.dirname(__file__) + '/resources'
+    run_script('connect to [{}];\n'
+               'list all graphs;'.format(path + '/test_dir'))
     lines = open(path + '/script_out.txt').read()
     assert capsys.readouterr().out == lines
 
 
 def test_named_pattern(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a S b S | ();'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    h = run_script('connect to [{}];\n'
+                   'S = a S b S | ();'.format(path + '/test_dir'))
     assert h.g.rules == {'Q0': [['a', 'S', 'b', 'S'], ['eps']], 'S': [['Q0']]}
 
 
 def test_named_pattern2():
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('S = a b + | ();')
-    f.close()
-    h = handle_script_from_file(temp.name)
+    h = run_script('S = a b + | ();')
     assert cyk(h.g, 'a b')
     assert cyk(h.g, 'a b b b b')
     assert cyk(h.g, '')
@@ -40,11 +36,7 @@ def test_named_pattern2():
 
 
 def test_named_pattern3():
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('S = a ? b;')
-    f.close()
-    h = handle_script_from_file(temp.name)
+    h = run_script('S = a ? b;')
     assert cyk(h.g, 'a b')
     assert cyk(h.g, 'b')
     assert not cyk(h.g, 'a')
@@ -53,11 +45,7 @@ def test_named_pattern3():
 
 
 def test_named_pattern4():
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('S = (a b)* | c;')
-    f.close()
-    h = handle_script_from_file(temp.name)
+    h = run_script('S = (a b)* | c;')
     assert cyk(h.g, 'a b')
     assert cyk(h.g, 'a b a b')
     assert cyk(h.g, 'c')
@@ -69,129 +57,89 @@ def test_named_pattern4():
 
 def test_select_exists(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a b | a C;'
-            'C = S b;'
-            'select exists u from [graph.txt] where (u) - S -> (_);'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];\n'
+               'S = a b | a C;'
+               'C = S b;'
+               'select exists u from [graph.txt] where (u) - S -> (_);'.format(path + '/test_dir'))
     assert capsys.readouterr().out == 'True\n'
 
 
 def test_select_exists2(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a b | a C;'
-            'C = S b;'
-            'select exists u from [graph.txt] where (_) - S -> (u);'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];\n'
+               'S = a b | a C;'
+               'C = S b;'
+               'select exists u from [graph.txt] where (_) - S -> (u);'.format(path + '/test_dir'))
     assert capsys.readouterr().out == 'True\n'
 
 
 def test_select_exists3(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a b | a C;'
-            'C = S b;'
-            'select exists (u, v) from [graph.txt] where (u) - S -> (v);'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];\n'
+               'S = a b | a C;'
+               'C = S b;'
+               'select exists (u, v) from [graph.txt] where (u) - S -> (v);'.format(path + '/test_dir'))
     assert capsys.readouterr().out == 'True\n'
 
 
 def test_select_exists4(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a b | a C;'
-            'C = S b;'
-            'select exists (u, v) from [graph.txt] where (u.ID = 1) - S -> (v);'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];\n'
+               'S = a b | a C;'
+               'C = S b;'
+               'select exists (u, v) from [graph.txt] where (u.ID = 1) - S -> (v);'.format(path + '/test_dir'))
     assert capsys.readouterr().out == 'True\n'
 
 
 def test_select_exists5(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a b | a C;'
-            'C = S b;'
-            'select exists (u, v) from [graph.txt] where (u.ID = 1) - S -> (v.ID = 3);'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];\n'
+               'S = a b | a C;'
+               'C = S b;'
+               'select exists (u, v) from [graph.txt] where (u.ID = 1) - S -> (v.ID = 3);'.format(path + '/test_dir'))
     assert capsys.readouterr().out == 'True\n'
 
 
 def test_select_exists6(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a b | a C;'
-            'C = S b;'
-            'select exists (u, v) from [graph.txt] where (u.ID = 1) - S -> (v.ID = 0);'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];\n'
+               'S = a b | a C;'
+               'C = S b;'
+               'select exists (u, v) from [graph.txt] where (u.ID = 1) - S -> (v.ID = 0);'.format(path + '/test_dir'))
     assert capsys.readouterr().out == 'False\n'
 
 
 def test_select_exists7(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a S| ();'
-            'select exists (u, v) from [graph.txt] where (u.ID = 1) - S b b b -> (v.ID = 3);'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];\n'
+               'S = a S| ();'
+               'select exists (u, v) from [graph.txt] where (u.ID = 1) - S b b b -> (v.ID = 3);'.format(path + '/test_dir'))
     assert capsys.readouterr().out == 'True\n'
 
 
 def test_select_exists8(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a S| ();'
-            'select exists (u, v) from [graph.txt] where (u.ID = 1) - S b b -> (v.ID = 3);'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];\n'
+               'S = a S| ();'
+               'select exists (u, v) from [graph.txt] where (u.ID = 1) - S b b -> (v.ID = 3);'.format(path + '/test_dir'))
     assert capsys.readouterr().out == 'False\n'
 
 
 def test_select_exists9(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];\n'
-            'S = a S | ();'
-            'select exists (u, v) from [graph.txt] where (u.ID = 1) - S b b -> (v.ID = 3);'
-            'S = a S b;'
-            'select exists (u, v) from [graph.txt] where (u.ID = 1) - S b b -> (v.ID = 3);'.format(path + '/test_dir'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];\n'
+               'S = a S | ();'
+               'select exists (u, v) from [graph.txt] where (u.ID = 1) - S b b -> (v.ID = 3);'
+               'S = a S b;'
+               'select exists (u, v) from [graph.txt] where (u.ID = 1) - S b b -> (v.ID = 3);'.format(path + '/test_dir'))
     assert capsys.readouterr().out == 'False\nTrue\n'
 
 
 def test_select_exists10(capsys):
     path = os.path.dirname(__file__) + '/resources'
-    temp = tempfile.NamedTemporaryFile()
-    f = open(temp.name, 'w')
-    f.write('connect to [{}];'
-            'S = a S | ();'
-            'select exists (u, v) from [graph.txt] where (u.ID = 1) - S c c c -> (v.ID = 3);'
-            'connect to [{}];'
-            'select exists (u, v) from [graph.txt] where (u.ID = 1) - S c c c -> (v.ID = 3);'.format(path + '/test_dir', path + '/test_dir2'))
-    f.close()
-    h = handle_script_from_file(temp.name)
+    run_script('connect to [{}];'
+               'S = a S | ();'
+               'select exists (u, v) from [graph.txt] where (u.ID = 1) - S c c c -> (v.ID = 3);'
+               'connect to [{}];'
+               'select exists (u, v) from [graph.txt] where (u.ID = 1) - S c c c -> (v.ID = 3);'.format(path + '/test_dir', path + '/test_dir2'))
     assert capsys.readouterr().out == 'False\nTrue\n'
